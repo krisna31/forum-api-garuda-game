205f1a7d1e09c9db635dd9b2d8097d21f0b60551
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2023-02-18 19:39:46 +0700

    Update GetThreadByIdUseCase.test.js
    Nilai expected value pada mock fungsi ini kurang tepat. Pada implementasi asli, fungsi ini seharusnya mengembalikan nilai array dari object yang juga terdapat properti

--- src/Applications/use_case/_test/GetThreadByIdUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadByIdUseCase.test.js
@@ -21,6 +21,8 @@ describe("GetThreadUseCase", () => {
     const expectedComments = [
       {
         id: "comment-123",
+        threadId: "thread-123",
+        owner: "user-123",
         username: "31",
         date: "2023",
         content: "ini isi komentar",
@@ -34,13 +36,18 @@ describe("GetThreadUseCase", () => {
         content: "ini isi balasan",
         date: "2023",
         username: "krisna",
+        owner: "user-123",
         comment_id: "comment-123",
         is_deleted: false,
       },
     ];
 
-    const mappedComments = expectedComments.map(({ is_deleted: deletedComment, ...rest }) => rest)[0];
-    const mappedReplies = expectedReplies.map(({ comment_id, is_deleted, ...rest }) => rest);
+    const mappedComments = expectedComments.map((
+      { is_deleted: deletedComment, threadId, owner, ...rest }
+      ) => rest)[0];
+    const mappedReplies = expectedReplies.map((
+      { owner, comment_id, is_deleted, ...rest }
+      ) => rest);
 
     const expectedCommentsAndReplies = [
       {
