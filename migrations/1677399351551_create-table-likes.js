d971cc682d60749fc99f5e6f8585e4a18b9db7cb
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2023-02-26 15:19:07 +0700

    Create 1677399351551_create-table-likes.js

---
+++ migrations/1677399351551_create-table-likes.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'cascade',
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('likes', 'fk_likes.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+};
+
+exports.down = pgm => {
+  pgm.dropTable('likes');
+};
